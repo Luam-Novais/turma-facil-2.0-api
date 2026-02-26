81a36de3f5e295cf1f9026d5dd77a8b64f89d49c
Luam-Novais <luampabl014@gmail.com>
2026-02-25 22:12:25 -0300

    fix: atualiza configuração do CORS para permitir acesso apenas do domínio de produção

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,12 @@ import cors from 'cors'
 
 const app = express()
 
-app.use(cors({
-origin: 'http://localhost:3000',
-credentials: true
-}))
+app.use(
+  cors({
+    origin: 'https://turma-facil-2-0.vercel.app/',
+    credentials: true,
+  }),
+);
 
 app.use(express.json())
 app.use(express.urlencoded())
